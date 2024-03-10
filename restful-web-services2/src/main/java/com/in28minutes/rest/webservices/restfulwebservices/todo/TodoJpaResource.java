33dfa86ab0b8f5aca91c01ca1d2844b9ad2e3f74
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2024-03-10 16:13:15 +0530

    ConnectingDatabase

--- restful-web-services2/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
+++ restful-web-services2/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
@@ -11,32 +11,39 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-public class TodoResource 
+import com.in28minutes.rest.webservices.restfulwebservices.todo.repository.TodoRespository;
+
+@RestController
+public class TodoJpaResource 
 {
 	private TodoService todoService;
+	private TodoRespository todoRespository;
 	
-	public TodoResource(TodoService todoService) {
+	public TodoJpaResource(TodoService todoService, TodoRespository todoRespository) {
 		this.todoService = todoService;
+		this.todoRespository = todoRespository;
 	}
 	
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> retrieveTodos(@PathVariable String username) 
 	{
-		return todoService.findByUsername(username);
+//		return todoService.findByUsername(username);
+		return todoRespository.findByUsername(username);
 		
 	}
 	@GetMapping("/users/{username}/todos/{id}")
 	public Todo retrieveTodo(@PathVariable String username, @PathVariable int id) 
 	{
-		return todoService.findById(id);
+//		return todoService.findById(id);
+		return todoRespository.findById(id).get();
 		
 	}
 	
 	@DeleteMapping("/users/{username}/todos/{id}")
 	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) 
 	{
-		todoService.deleteById(id);
+//		todoService.deleteById(id);
+		todoRespository.deleteById(id);
 		return ResponseEntity.noContent().build();
 		
 	}
@@ -44,7 +51,8 @@ public class TodoResource
 	@PutMapping("/users/{username}/todos/{id}")
 	public Todo updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo) 
 	{
-		todoService.updateTodo(todo);
+//		todoService.updateTodo(todo);
+		todoRespository.save(todo);
 		return todo;
 		
 	}
@@ -52,8 +60,11 @@ public class TodoResource
 	@PostMapping("/users/{username}/todos")
 	public Todo createTodo(@PathVariable String username, @RequestBody Todo todo) 
 	{
-		Todo createdTodo = todoService.addTodo(username,todo.getDescription(),todo.getTargetDate(),todo.isDone());
-		return createdTodo;
+		todo.setUsername(username);
+		todo.setId(null);
+		return todoRespository.save(todo);
+//		Todo createdTodo = todoService.addTodo(username,todo.getDescription(),todo.getTargetDate(),todo.isDone());
+//		return createdTodo;
 		
 	}
 
--- restful-web-services2/src/main/resources/data.sql
+++ restful-web-services2/src/main/resources/data.sql
@@ -1,26 +1,8 @@
-insert into user_details(birth_date,id,name)
-values(current_date(),1001 , 'Luffy');
+insert into todo(done, id, target_date, description, username)
+values(false, 1, CURRENT_DATE(), 'Utho', 'Zoro');
 
-insert into user_details(birth_date,id,name)
-values(current_date(),1002 , 'Zoro');
+insert into todo(done, id, target_date, description, username)
+values(false, 2, CURRENT_DATE(), 'Ghum ke Aaunga', 'Zoro');
 
-insert into user_details(birth_date,id,name)
-values(current_date(),1003 , 'Sanji');
-
-insert into post(id,user_id,description)
-values(2001,1001,'Captain');
-
-insert into post(id,user_id,description)
-values(2002,1002,'First Commander');
-
-insert into post(id,user_id,description)
-values(2003,1003,'Chef');
-
-insert into post(id,user_id,description)
-values(2004,1002,'Direction Less');
-
-insert into post(id,user_id,description)
-values(2005,1002,'SleepyHead');
-
-insert into post(id,user_id,description)
-values(2006,1003,'Mannerism');
+insert into todo(done, id, target_date, description, username)
+values(false, 3, CURRENT_DATE(), 'Piyo aur so jao', 'Zoro');
